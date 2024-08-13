daad9a3a1adde46e402f7f1e552f9555c655fb8e
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2024-08-13 22:53:17 +0700

    add helmet

--- src/index.js
+++ src/index.js
@@ -2,15 +2,29 @@ import "animate.css/animate.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Helmet } from "react-helmet";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Helmet>
+      <title>Maulana Arif</title>
+      <meta
+        name="description"
+        content="Maulana Arif's personal website. A web developer and software engineer."
+      />
+      <meta
+        name="keywords"
+        content="Maulana Arif, web developer, software engineer"
+      />
+      <meta name="author" content="Maulana Arif" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#000000" />
+      <App />
+    </Helmet>
   </React.StrictMode>,
   document.getElementById("root")
 );
